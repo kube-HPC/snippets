90d440a303f2652c6d3a6f74f98edba99faab3f1
yehiyam <yehiyam@users.noreply.github.com>
2020-06-28 18:18:41 +0300

    update for helm 3

--- scripts/getHkubeVersion.sh
+++ scripts/getHkubeVersion.sh
@@ -6,36 +6,37 @@ if [ -z $DEV_VERSION ]; then
 else
   HKUBE_CHART_REPO="hkube-dev/hkube"
 fi
-LATEST_VERSION=$(helm search hkube|grep "${HKUBE_CHART_REPO}" | awk '{print $2}')
+LATEST_VERSION=$(helm search repo hkube|grep "${HKUBE_CHART_REPO}" | awk '{print $2}')
 VERSION=${VERSION:-$LATEST_VERSION}
 echo downloading version $VERSION
 
 DIR=${BASE_DIR}/hkube-${VERSION}
 mkdir -p ${DIR}
 cd ${DIR}
-helm fetch --untar ${HKUBE_CHART_REPO} --version ${VERSION}
+helm pull --untar ${HKUBE_CHART_REPO} --version ${VERSION}
 mkdir -p dockers
 # cp $HOME/dev/hkube/image-exprort-import/image-export-import .
 curl -Lo image-export-import https://github.com/kube-HPC/image-exprort-import/releases/download/$(curl -s https://api.github.com/repos/kube-HPC/image-exprort-import/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')/image-export-import \
 && chmod +x image-export-import
 
+curl -Lo hkubectl https://github.com/kube-HPC/hkubectl/releases/download/$(curl -s https://api.github.com/repos/kube-HPC/hkubectl/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')/hkubectl-linux \
+&& chmod +x hkubectl
 if [ ! -z $PREV_VERSION ]
 then
-  helm fetch --untar ${HKUBE_CHART_REPO} --untardir hkube-${PREV_VERSION} --version ${PREV_VERSION}
+  helm pull --untar ${HKUBE_CHART_REPO} --untardir hkube-${PREV_VERSION} --version ${PREV_VERSION}
   ./image-export-import export --path $PWD --semver ./hkube/values.yaml --prevVersion  ./hkube-${PREV_VERSION}/hkube/values.yaml
-  ./image-export-import exportThirdparty --path $PWD --chartPath ./hkube/charts/thirdparty/ --prevChartPath ./hkube-${PREV_VERSION}/hkube/charts/thirdparty/ --production true
+  ./image-export-import exportThirdparty --path $PWD --chartPath ./hkube/ --prevChartPath ./hkube-${PREV_VERSION}/hkube/ --options "etcd-operator.enable=true,jaeger.enable=true,nginx-ingress.enable=true"
   rm -rf hkube-${PREV_VERSION}
 else
   ./image-export-import export --path $PWD --semver ./hkube/values.yaml
-  ./image-export-import exportThirdparty --path $PWD --chartPath ./hkube/charts/thirdparty/ --production true
-  ./image-export-import exportThirdparty --path $PWD --chartPath ./hkube/charts/thirdparty/ --production false
+  ./image-export-import exportThirdparty --path $PWD --chartPath ./hkube/ --options "etcd-operator.enable=true,jaeger.enable=true,nginx-ingress.enable=true"
 fi
 
 sleep 5
 mv ${VERSION} dockers/hkube
 mv thirdparty dockers/
 echo compressing 
-tar cfz hkube-${VERSION}.tgz hkube dockers image-export-import
+tar cfz hkube-${VERSION}.tgz hkube dockers image-export-import hkubectl
 if [ ! -z $OBFUSCATE ]
 then
   base64 hkube-${VERSION}.tgz >hkube-${VERSION}.tgz.b64
